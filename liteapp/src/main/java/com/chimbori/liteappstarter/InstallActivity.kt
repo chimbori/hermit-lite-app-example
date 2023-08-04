9dfb4ae77cb6d6d6d2a6d6faff0fe81e9f7e0582
Chimbori <hello@chimbori.com>
2023-08-04 16:27:27 -0700

    Merge code from module `com.chimbori.liteappstarter` into this example app inline

---
+++ liteapp/src/main/java/com/chimbori/liteappstarter/InstallActivity.kt
@@ -0,0 +1,58 @@
+package com.chimbori.liteappstarter
+
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.Intent.ACTION_VIEW
+import android.net.Uri
+import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES.N
+import android.os.Bundle
+import android.text.Html
+import android.text.Html.FROM_HTML_MODE_LEGACY
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import com.chimbori.liteappstarter.LiteAppStarterActivity.Companion.HERMIT_PACKAGE_NAME
+import org.example.liteapp.R
+
+class InstallActivity : Activity(), View.OnClickListener {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_install_dialog)
+
+    findViewById<TextView>(R.id.install_dialog_description).text = if (SDK_INT >= N) {
+      Html.fromHtml(getString(R.string.hermit_description), FROM_HTML_MODE_LEGACY)
+    } else {
+      @Suppress("DEPRECATION")
+      Html.fromHtml(getString(R.string.hermit_description))
+    }
+    findViewById<View>(R.id.install_dialog_install_button).setOnClickListener(this)
+    findViewById<View>(R.id.install_dialog_cancel_button).setOnClickListener(this)
+  }
+
+  override fun onClick(view: View) {
+    when (view.id) {
+      R.id.install_dialog_install_button -> {
+        try {
+          startActivity(Intent(ACTION_VIEW, Uri.parse("market://details?id=$HERMIT_PACKAGE_NAME")))
+        } catch (e: ActivityNotFoundException) {
+          Toast.makeText(applicationContext, R.string.error_play_store, LENGTH_LONG).show()
+          try {
+            startActivity(Intent(ACTION_VIEW, Uri.parse(HERMIT_DOWNLOAD_LOCATION)))
+          } catch (e: ActivityNotFoundException) {
+            Toast.makeText(applicationContext, getString(R.string.error_no_app_available), LENGTH_LONG).show()
+          }
+        }
+        finishAndRemoveTask()
+      }
+      R.id.install_dialog_cancel_button -> finishAndRemoveTask()
+    }
+  }
+
+  companion object {
+    /** URL where Hermit can be downloaded from directly as an APK. Used on devices where Play Store is not available.  */
+    private const val HERMIT_DOWNLOAD_LOCATION = "https://hermit.chimbori.com/downloads"
+  }
+}
---
+++ liteapp/src/main/java/com/chimbori/liteappstarter/LiteAppStarterActivity.kt
@@ -0,0 +1,35 @@
+package com.chimbori.liteappstarter
+
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.Intent.ACTION_VIEW
+import android.net.Uri
+import android.os.Bundle
+import org.example.liteapp.R
+
+/** The main code for this app, served as a library. No need to make any changes here. */
+class LiteAppStarterActivity : Activity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    launchLiteApp(this, getString(R.string.start_url))
+    finish()
+  }
+
+  companion object {
+    private fun launchLiteApp(activity: Activity, startUrl: String) = try {
+      activity.startActivity(createLiteAppIntent(startUrl))
+    } catch (e: ActivityNotFoundException) {
+      activity.startActivity(Intent(activity, InstallActivity::class.java))
+    }
+
+    internal fun createLiteAppIntent(startUrl: String) =
+      Intent(ACTION_VIEW, Uri.parse(startUrl)).setClassName(HERMIT_PACKAGE_NAME, HERMIT_ACTIVITY_NAME)
+
+    /** Package name of the Hermit app.  */
+    const val HERMIT_PACKAGE_NAME = "com.chimbori.hermitcrab"
+
+    /** Component name of the deep-linked Activity in Hermit to use to open a Lite App.  */
+    private const val HERMIT_ACTIVITY_NAME = "com.chimbori.hermitcrab.WebActivity"
+  }
+}
