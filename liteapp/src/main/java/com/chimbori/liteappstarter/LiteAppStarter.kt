07c46ae9c7b9d1787f22d0bbe603f4dbde9685c4
Chimbori <hello@chimbori.com>
2023-08-04 17:01:46 -0700

    Merge both classes into a single Kotlin file for enhanced readability

--- liteapp/src/main/java/com/chimbori/liteappstarter/LiteAppStarter.kt
+++ liteapp/src/main/java/com/chimbori/liteappstarter/LiteAppStarter.kt
@@ -10,9 +10,25 @@ import android.text.Html
 import android.text.Html.FROM_HTML_MODE_LEGACY
 import android.widget.Toast
 import android.widget.Toast.LENGTH_LONG
-import com.chimbori.liteappstarter.LiteAppStarterActivity.Companion.HERMIT_PACKAGE_NAME
 import com.chimbori.liteappstarter.databinding.ActivityInstallDialogBinding
 
+/** The main code for this app. No need to make any changes here. */
+class LiteAppStarterActivity : Activity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    try {
+      startActivity(
+        Intent(ACTION_VIEW, Uri.parse(getString(R.string.start_url)))
+          .setClassName(HERMIT_PACKAGE_NAME, HERMIT_ACTIVITY_NAME)
+      )
+    } catch (e: ActivityNotFoundException) {
+      startActivity(Intent(this, InstallActivity::class.java))
+    }
+    finish()
+  }
+}
+
+/** If Hermit is not installed, this shows a message to the user to go download it. */
 class InstallActivity : Activity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -20,7 +36,9 @@ class InstallActivity : Activity() {
     val binding = ActivityInstallDialogBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
-    binding.installDialogDescription.text = Html.fromHtml(getString(R.string.hermit_description), FROM_HTML_MODE_LEGACY)
+    binding.installDialogDescription.text = Html.fromHtml(
+      getString(R.string.hermit_description), FROM_HTML_MODE_LEGACY
+    )
     binding.installDialogInstallButton.setOnClickListener {
       try {
         startActivity(Intent(ACTION_VIEW, Uri.parse("market://details?id=$HERMIT_PACKAGE_NAME")))
@@ -38,9 +56,13 @@ class InstallActivity : Activity() {
       finishAndRemoveTask()
     }
   }
-
-  companion object {
-    /** URL where Hermit can be downloaded from directly as an APK. Used on devices where Play Store is not available.  */
-    private const val HERMIT_DOWNLOAD_LOCATION = "https://hermit.chimbori.com/downloads"
-  }
 }
+
+/** Package name of the Hermit app. */
+private const val HERMIT_PACKAGE_NAME = "com.chimbori.hermitcrab"
+
+/** Component name of the deep-linked Activity in Hermit to use to open a Lite App. */
+private const val HERMIT_ACTIVITY_NAME = "com.chimbori.hermitcrab.WebActivity"
+
+/** URL where Hermit can be downloaded from directly as an APK. Used on devices where Play Store is not available. */
+private const val HERMIT_DOWNLOAD_LOCATION = "https://hermit.chimbori.com/downloads"
